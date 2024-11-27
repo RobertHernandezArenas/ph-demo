48a3404b83d687f0a80a47abab0a8bbd690f7646
Robert Hernandez Arenas <airbusjayrobert@gmail.com>
2024-11-27 10:36:12 +0100

    ph demo web

---
+++ assets/PlayersView.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./index.js";import{g as s,f as o,q as t}from"./vue.js";const c={},r={class:"about"},a=t("h1",null,"Players",-1),_=[a];function n(i,l){return o(),s("div",r,_)}const m=e(c,[["render",n]]);export{m as default};
+//# sourceMappingURL=PlayersView.js.map
