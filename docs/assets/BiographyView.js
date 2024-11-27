726a0d8f200ed2329795908bfc53148a0904257c
Robert Hernandez Arenas <airbusjayrobert@gmail.com>
2024-11-27 12:33:50 +0100

    ph demo web

---
+++ docs/assets/BiographyView.js
@@ -0,0 +1,2 @@
+import{q as e,o,e as t}from"./vue.js";const s={class:"biography"},_={__name:"BiographyView",setup(a){return e(()=>{}),(c,n)=>(o(),t("section",s))}};export{_ as default};
+//# sourceMappingURL=BiographyView.js.map
---
+++ docs/assets/GalleryView.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./index.js";import{o,e as r}from"./vue.js";const c={},t={class:"gallery"};function s(a,n){return o(),r("div",t)}const f=e(c,[["render",s]]);export{f as default};
+//# sourceMappingURL=GalleryView.js.map
